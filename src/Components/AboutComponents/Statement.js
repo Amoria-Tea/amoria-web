294d262574606d06b06eadbd3234bca2822f8848
Mike Liu <mliu@etsy.com>
2022-08-17 21:27:38 -0400

    fade in pages

--- src/Components/AboutComponents/Statement.js
+++ src/Components/AboutComponents/Statement.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import 'Components/fade-in.css';
 function Statement(props) {
 // ToDo
 // For this component make it responsive: Use below link for reference 
@@ -7,20 +7,20 @@ function Statement(props) {
 
     return (
         <>
-        <div className="about-container">
-        <div class='flex flex-col sm:flex-row '>
-        <div class="ml-1 text-sm sm:text-xl md:text-2xl lg:text-4xl">
-        <br/>
-        <strong className='poppins-bolded-responsive'> <span className='poppins-bolded-responsive'>We are Amoria Tea..</span> </strong> 
-        <br/>
-        <p class="mb-2 sm:-mt-4">
-        an up and coming environmentally conscious and consumer health conscious bubble tea shop based in NJ. 
-        </p>
-        </div>
-        <div className="man-making-tea-container">
-        <img className='tea-making-man' src={"https://i.imgur.com/p8d4neA.jpeg"} alt="man making tea"/>
-        </div>
-        </div>
+        <div className="about-container fade-in">
+            <div class='flex flex-col sm:flex-row '>
+                <div class="ml-1 text-sm sm:text-xl md:text-2xl lg:text-4xl">
+                    <br/>
+                        <strong className='poppins-bolded-responsive'> <span className='poppins-bolded-responsive'>We are Amoria Tea..</span> </strong> 
+                    <br/>
+                        <p class="mb-2 sm:-mt-4">
+                        an up and coming environmentally conscious and consumer health conscious bubble tea shop based in NJ. 
+                        </p>
+                </div>
+                <div className="man-making-tea-container">
+                    <img className='tea-making-man' src={"https://i.imgur.com/p8d4neA.jpeg"} alt="man making tea"/>
+                </div>
+            </div>
         </div>
         </>
     );
--- src/Components/MenuComponents/Menu.js
+++ src/Components/MenuComponents/Menu.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import 'Components/fade-in.css';
 function Menu(props) {
     return (
-        <div>
+        <div className="menu-container fade-in">
         <div class="flex justify-center">
         <img class="w-screen h-full sm:w-full sm:h-full md:w-10/12 md:h-10/12 lg:w-8/12 lg:h-8/12 mt-9 mb-9 " src={"https://i.imgur.com/oolIQW4.png"} alt="Menu"/>
         </div>
