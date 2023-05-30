bfd7269867b1b527ddcac37740062006f558be24
Sungmin Lee <sungminlee417@gmail.com>
2023-05-30 10:03:29 -0700

    added font bolds on nav bar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,51 +1,82 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 function Nav(props) {
-
   const [underline, SetUnderLine] = useState(false);
 
   const onThisNavComponent = () => {
-    SetUnderLine(!underline);  
-    console.log(SetUnderLine(!underline))
-  }
+    SetUnderLine(!underline);
+    console.log(SetUnderLine(!underline));
+  };
 
-return (
-<header>
-  <div className="max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
-    <div className="menu">
-      <div className="md:block">
-        <nav aria-labelledby="header-navigation">
-          <h2 className="sr-only" id="header-navigation">Header navigation</h2>
-        <div className="nav-ul-container">
-          <ul className="flex items-center gap-7 text-sm sm:text-2xl">
-            <li className="">
-              <Link onClick={onThisNavComponent} to="/Menu" className="transition hover:text-black/75 active:font-extrabold">
-                Menu
-              </Link>
-            </li>
+  return (
+    <header>
+      <div className="max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
+        <div className="menu">
+          <div className="md:block">
+            <nav aria-labelledby="header-navigation">
+              <h2 className="sr-only" id="header-navigation">
+                Header navigation
+              </h2>
+              <div className="nav-ul-container">
+                <ul className="flex items-center gap-7 text-sm sm:text-2xl">
+                  <li className="">
+                    <NavLink
+                      onClick={onThisNavComponent}
+                      to="/Menu"
+                      // className=" "
+                      className={({ isActive }) =>
+                        isActive
+                          ? "font-bold"
+                          : "transition hover:text-black/75 active:font-extrabold"
+                      }
+                    >
+                      Menu
+                    </NavLink>
+                  </li>
 
-            <li className="">
-              <Link to="/about" className="transition hover:text-black/75">
-                About
-              </Link>
-            </li>
-            <li className="">
-              <Link to="/visitus" className="transition hover:text-black/75">
-                Visit Us
-              </Link>
-            </li>
-            <li className="">
-              <Link to="/contact" className="transition hover:text-black/75">
-               Contact 
-              </Link>
-            </li>
-          </ul>
-        </div>
-        </nav>
-      </div>
-      
-      {/* <div class="flex items-center gap-4">
+                  <li className="">
+                    <NavLink
+                      to="/about"
+                      className={({ isActive }) =>
+                        isActive
+                          ? "font-bold"
+                          : "transition hover:text-black/75 active:font-extrabold"
+                      }
+                    >
+                      About
+                    </NavLink>
+                  </li>
+                  <li className="">
+                    <NavLink
+                      to="/visitus"
+                      className={({ isActive }) =>
+                        isActive
+                          ? "font-bold"
+                          : "transition hover:text-black/75 active:font-extrabold"
+                      }
+                    >
+                      Visit Us
+                    </NavLink>
+                  </li>
+                  <li className="">
+                    <NavLink
+                      to="/contact"
+                      className={({ isActive }) =>
+                        isActive
+                          ? "font-bold"
+                          : "transition hover:text-black/75 active:font-extrabold"
+                      }
+                    >
+                      Contact
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+
+          {/* <div class="flex items-center gap-4">
         <div class="block md:hidden">
           <button
             className="hamburger-icon"
@@ -67,10 +98,10 @@ return (
           </button>
         </div>
       </div> */}
-    </div>
-  </div>
-</header>
-    );
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Nav;
