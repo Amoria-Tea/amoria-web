99a54945ba9ac98ddfc7a89eb42bd151754453d5
Andrew Kal <andrewkalgit@gmail.com>
2023-06-01 19:32:34 -0400

    custom calendar component

--- src/Components/VisitComponents/Visit.js
+++ src/Components/VisitComponents/Visit.js
@@ -1,12 +1,27 @@
-
+import { useState } from "react";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
+import './visit.css'
 //scroll into view then fade in 
 
 function Visit(props) {
+
+    const [date, setDate] = useState(new Date());
+    
+
     return (
-        <div>
+     <>
+     {/* <div>
 <iframe src="https://embed.styledcalendar.com/#nA5fTtBlRGyAHYruKtIA" height="1000px" width="100%"></iframe>
-        </div>
-    );
+    </div> */}
+    <div>
+    <div className='calendar-container mb-12 mt-12'>
+        <Calendar onChange={setDate} value={date}
+        />
+      </div>
+    </div>
+    </>
+  );
 }
 
 export default Visit;
