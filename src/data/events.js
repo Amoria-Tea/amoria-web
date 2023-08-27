e4ad14cdc9c94d05e08d29f95b3afa72d964fe76
Michael Liu <mikeliumeng@gmail.com>
2023-08-27 17:53:39 -0400

    updated events

--- src/data/events.js
+++ src/data/events.js
@@ -1,12 +1,21 @@
 const events = [
   {
-    name: "July 4th Street Festival",
-    date: new Date(2023, 6, 4, 12, 0), // Start date
-    end: new Date(2023, 6, 4, 22, 0), // End date
-    location: "Montgomery Street (Warren to Hudson), Jersey City, NJ, 07302",
-    description: "Come for a full day (and night) of delicious boba, food, vendors and a fireworks show to end the night!",
+    name: "A Taste of North Jersey",
+    date: new Date(2023, 8, 17, 11, 0), // Start date
+    end: new Date(2023, 8, 17, 17, 0), // End date
+    location: "18 Label St, Montclair, NJ 07042",
+    description: "Introducing NJ.com`'s 1st food festival: Jersey Eats, A Taste of New Jersey",
+    bannerImg: "https://assets.swoogo.com/uploads/medium/2708188-649c540ca8e09.png",
+    eventLink: "https://events.nj.com/jersey-eats-taste-of-nj-2023/montclair?ref=Article#Info",
+  },
+  {
+    name: "Jersey City Night Market",
+    date: new Date(2023, 9, 7, 15, 0), // Start date
+    end: new Date(2023, 9, 7, 21, 0), // End date
+    location: "179 Montgomery Street, Jersey City",
+    description: "Come for a full night of delicious boba, food, and artisanal vendors!",
     bannerImg: "blah",
-    eventLink: "https://www.instagram.com/p/CtiQR8nOo1s/",
+    eventLink: "https://www.midnightmarketjc.com/jcnightmarket",
   },
 ];
 
